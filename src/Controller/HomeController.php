a25ae3ed96f4e5e75e88def98d127fef5beb8587
MattCoy <contact@le-web-developpeur.com>
2018-12-18 10:04:49 +0100

    Ajout de twig : render() et passage de variables

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,26 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
+
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function home(){
+
+        //on crée une variable
+        $nom = 'Toto';
+        //on la passe en paramètre de notre méthode render()
+        return $this->render('index.html.twig', array('nom' => $nom));
+
+    }
+    //...
 
     /**
      * @Route("/bonjour")
