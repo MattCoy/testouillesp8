77c2b404aa7ad5b1aaf71a636f0401ac6d1fdf5f
MattCoy <contact@le-web-developpeur.com>
2018-12-27 21:13:20 +0100

    Category : update , delete + param converter & flashbags

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -47,4 +47,74 @@ class CategoryController extends AbstractController
 
         return $this->render('category/last.five.html.twig', ['categories' => $categories]);
     }
+
+    /**
+     * @Route("/categorie/update/{id}",
+     *     name="categorie_update",
+     *     requirements={"id":"\d+"}
+     * )
+     */
+    public function updateCategorie(Category $category)
+    {
+        /* Ici on utilise une fonctionnalité très utile de symfony : le ParamConverter
+        Alors que l'on devrait récupérer en argument $id, l'id de la catégorie,
+        on indique à Symfony que l'on veut récupérer l'entité Categorie:
+        le ParamConverter va comprendre cela et va lui même faire la requête find($id),
+        ce qui nous évite d'écrire:
+        $article = $this->getDoctrine()
+            ->getRepository(Categorie::class)
+            ->find($id);
+        Si aucun article n'est trouvé, une erreur 404 est générée.
+        */
+
+        //Ensuite je peut modifier ma catégorie
+        $category->setLibelle('catégorie modifiée');
+
+        //récupération du manager
+        $entityManager = $this->getDoctrine()->getManager();
+
+        //ici pas besoin de faire $entityManager->persist($categorie);
+        //car doctrine a déjà en mémoire cette entité, puisqu'elle l'a récupéré dans la base
+
+        $entityManager->flush();
+        //à ce moment, doctrine sait que $categorie existe déjà dans la base et va donc faire un update au lieu d'un insert !
+
+        //On va utiliser une autre fonctionnalité très utile de Symfony : les messages flashs:
+        // Ce sont des messages stockés en session et qui sont ensuite effaçés dès qu'ils sont affichés,
+        //de sorte qu'il n'apparaissent qu'une seule fois
+        $this->addFlash(
+            'success',
+            'Catégorie modifiée !'
+        );
+
+        //on redirige sur la liste des 5 dernières catégories
+        return $this->redirectToRoute('categorie_last5');
+    }
+
+    /**
+     * @Route("/categorie/delete/{id}",
+     *     name="categorie_delete",
+     *     requirements={"id":"\d+"}
+     * )
+     */
+    public function deleteCategorie(Category $category)
+    {
+        // le ParamConverter convertit autoamtiquement l'id en objet Categorie
+
+        //récupération du manager
+        $entityManager = $this->getDoctrine()->getManager();
+
+        //Je veux supprimer cette catégorie
+        $entityManager->remove($category);
+
+        //j'oexecute les requêtes
+        $entityManager->flush();
+        $this->addFlash(
+            'warning',
+            'Catégorie supprimée !'
+        );
+
+        //on redirige sur la liste des 5 dernières catégories
+        return $this->redirectToRoute('categorie_last5');
+    }
 }
